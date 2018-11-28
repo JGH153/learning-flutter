89904941fcdeec1dd6a0511ea906cb7be110ec95
Jan Greger Hemb <jan.greger@gmail.com>
2018-11-28 13:21:12 +0100

    More refactoring and minor features

--- first_flut/lib/pages/tabs_admin/product_list.page.dart
+++ first_flut/lib/pages/tabs_admin/product_list.page.dart
@@ -5,39 +5,52 @@ import './products_edit.page.dart';
 class ProductListPage extends StatelessWidget {
   final List<Map<String, dynamic>> products;
   final Function updateProduct;
+  final Function deleteProduct;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
+
+  Widget _buildEditButton(BuildContext context, int index) {
+    return IconButton(
+      icon: Icon(Icons.edit),
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (BuildContext context) {
+            return ProductEditPage(
+              product: products[index],
+              productIndex: index,
+              updateProduct: updateProduct,
+            );
+          }),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
         return Dismissible(
-          background: Container(color: Colors.red,),
+          direction: DismissDirection.endToStart,
+          onDismissed: (DismissDirection direction) {
+            print(direction);
+            if (direction == DismissDirection.endToStart) {
+              deleteProduct(index);
+            }
+          },
+          background: Container(
+            color: Colors.red,
+          ),
           key: Key(products[index]['title']),
           child: Column(
             children: <Widget>[
               ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage(products[index]['image']),
-                ),
-                title: Text(products[index]['title']),
-                subtitle: Text('\$${products[index]["price"].toString()}'),
-                trailing: IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (BuildContext context) {
-                        return ProductEditPage(
-                          product: products[index],
-                          productIndex: index,
-                          updateProduct: updateProduct,
-                        );
-                      }),
-                    );
-                  },
-                ),
-              ),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(products[index]['image']),
+                  ),
+                  title: Text(products[index]['title']),
+                  subtitle: Text('\$${products[index]["price"].toString()}'),
+                  trailing: _buildEditButton(context, index)),
               Divider(),
             ],
           ),
